0a6687cee0d72dae736e70eee71312ea7ffe6f65
Sara Stanton <sara.mpa13@gmail.com>
2018-08-30 22:58:50 -0500

    added book, author, user, genre resources

---
+++ db/migrate/20180831034139_create_books.rb
@@ -0,0 +1,11 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.string :author
+      t.string :genre
+      t.integer :page_count
+
+      t.timestamps
+    end
+  end
+end
