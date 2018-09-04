b43cca4d8575945eebeb1dcf0193173379f8c8fe
Sara Stanton <sara.mpa13@gmail.com>
2018-09-04 01:35:15 -0500

    further additions to show and index pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render 'profile'
+  end
+
+  def index
+  end
+
 end
