bd605262106139a5a64fcd55ab81a42273d60dfc
Sara Stanton <sara.mpa13@gmail.com>
2018-09-07 03:08:22 -0500

    added fix for Goodreads OmniAuth

---
+++ lib/strategies/omniauth-goodreads.rb
@@ -0,0 +1,6 @@
+require "omniauth-goodreads/version"
+require 'omniauth/strategies/goodreads'
+
+
+
+# source: https://github.com/sandboxws/omniauth-goodreads
---
+++ lib/strategies/omniauth-goodreads/version.rb
@@ -0,0 +1,8 @@
+module OmniAuth
+  module Goodreads
+    VERSION = "1.0.0"
+  end
+end
+
+
+# source: https://github.com/sandboxws/omniauth-goodreads
