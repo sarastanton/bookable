8b11cdf7b3c540e7c8ab8a099c8191db441c13f0
Sara Stanton <sara.mpa13@gmail.com>
2018-09-12 03:12:37 -0500

    set up Add Rating form

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,7 +1,21 @@
 class RatingsController < ApplicationController
 
   def new
-    @rating = Rating.new
+    @rating = Rating.new(book_id: params[:book_id], user_id: helpers.current_user.id)
+    @book = Book.find(params[:book_id])
   end
-  
+
+  def create
+    @book = Book.find(params[:book_id])
+    @user = User.find(helpers.current_user.id)
+    @rating = Rating.create(rating_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def rating_params
+    params.require(:rating).permit(:user_id, :book_id, :value)
+  end
+
 end
