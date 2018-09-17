32beef92a5f01b958b8c22798c60b6f30a4bdeeb
Sara Stanton <sara.mpa13@gmail.com>
2018-09-17 04:29:50 -0500

    styling on navbar

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,7 +3,7 @@ module SessionsHelper
   def login_status_display
 		if logged_in?
 			username = User.find_by(id: session[:user_id]).username
-			login_status = "You are logged in as #{username}"
+			login_status = "You are logged in as #{username} "
 		else
 			login_status = "You are NOT logged in"
 		end
